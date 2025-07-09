bcc59a2bbd1e2ea7cc362ca0d8bd9fdaa63cf3ae
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-07-09 16:32:21 +0300

    Add

--- MvcStok/Controllers/HakkindaController.cs
+++ MvcStok/Controllers/HakkindaController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcStok.Models.Entity;
 
 namespace MvcStok.Controllers
 {
-    public class HakkındaController : Controller
+    public class HakkindaController : Controller
     {
         // GET: Hakkında
         public ActionResult Index()
